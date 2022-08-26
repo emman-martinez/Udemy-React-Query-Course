e55f687fc82e9fe443d5643464f63ba4a9b4f756
Emmanuel Martínez <emmanuel.martinez@medtrainer.com>
2022-08-26 12:38:41 -0500

    Code Quiz! Infinite Species

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -3,16 +3,21 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { useState } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [tab, setTab] = useState("People");
+  const newTab = tab === "People" ? "Species" : "People";
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
         <h1>Infinite SWAPI</h1>
-        <InfinitePeople />
-        {/* <InfiniteSpecies /> */}
+        <button className="btn-change" onClick={() => setTab(newTab)}>
+          Change to {newTab}
+        </button>
+        {tab === "People" ? <InfinitePeople /> : <InfiniteSpecies />}
         <ReactQueryDevtools />
       </div>
     </QueryClientProvider>
